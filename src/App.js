9872682e23093dd15501c49e067664940caf31a7
Sergey Yevtushok <ggazila@gmail.com>
2021-11-30 20:47:43 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ const App = () => {
     const [news, setNews] = useState([]);
     const [searchQuery, setSearchQuery] = useState("react")
     const [url, setUrl] = useState("https://hn.algolia.com/api/v1/search?query=react")
+    const [loading, setLoading] = useState(false);
+
     const fetchNews = () => {
+        setLoading(true)
         fetch(`${url}`)
             .then(result => result.json())
-            .then(data => setNews(data.hits))
+            .then(data => {
+                setNews(data.hits);
+                setLoading(false)
+            })
             .catch(error => console.log(error))
     }
 
@@ -23,13 +29,21 @@ const App = () => {
         e.preventDefault();
         setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
     }
+
+    const showLoading = () => (loading ? <h2>Loading...</h2> : "")
+
+    const searchForm = () => (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={searchQuery} onChange={handleChange}/>
+            <button>Search</button>
+        </form>
+    )
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={searchQuery} onChange={handleChange}/>
-                <button>Search</button>
-            </form>
             <h2>News</h2>
+
+            {showLoading()}
+            {searchForm()}
             {news.map((n, i) => (<p key={i}>{n.title}</p>))}
         </div>
     )
